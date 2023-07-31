f3ceb7c882762e13369555bc440dee20ff8eca39
Muhammad Nurhuda Hendrian <mnurhuda009@gmail.com>
2023-08-01 00:01:52 +0700

    create api url

---
+++ lib/config/api.dart
@@ -0,0 +1,11 @@
+class Api {
+  static const _baseUrl = "http://192.168.1.6/discuss_app_backend";
+  static const user = "$_baseUrl/user";
+  static const topic = "$_baseUrl/topic";
+  static const follow = "$_baseUrl/follow";
+  static const comment = "$_baseUrl/comment";
+
+  static const imageUser = "$_baseUrl/images/user";
+  static const imageTopic = "$_baseUrl/images/topic";
+  static const imageComment = "$_baseUrl/images/comment";
+}
