d9303b6cdc51f8f5c00881ee9ee72e2ac9f6031a
Muhammad Nurhuda Hendrian <mnurhuda009@gmail.com>
2023-08-01 00:02:17 +0700

    create app format for number and date

---
+++ lib/config/app_format.dart
@@ -0,0 +1,39 @@
+import 'package:intl/intl.dart';
+
+class AppFormat {
+  static String infoNumber(double number) {
+    return NumberFormat.compact().format(number);
+  }
+
+  static String fullDateTine(String date) {
+    return DateFormat('EEE, d MM yy | HH:mm').format(DateTime.parse(date));
+  }
+
+  static String publish(String date) {
+    DateTime dateTime = DateTime.parse(date);
+    DateTime now = DateTime.now();
+    DateTime yesterday = now.subtract(const Duration(days: 1));
+
+    if (dateTime.isBefore(yesterday)) {
+      return fullDateTine(date);
+    } else {
+      int hour = now.difference(dateTime).inHours;
+      if (hour == 1) {
+        return '$hour hour';
+      } else if (hour > 1) {
+        return '$hour hours';
+      } else {
+        int minute = now.difference(dateTime).inMinutes;
+        if (minute > 1) {
+          return '$minute minutes';
+        } else if (minute == 1) {
+          return '$minute minute';
+        } else {
+          int second = now.difference(dateTime).inSeconds;
+          second = second < 0 ? 1 : second;
+          return '$second seconds';
+        }
+      }
+    }
+  }
+}
