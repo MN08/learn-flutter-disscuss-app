3bac06da04e9757b9f09185bb70901c281d85fec
Muhammad Nurhuda Hendrian <mnurhuda009@gmail.com>
2023-08-02 11:57:57 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:learn_flutter_discuss_app/config/app_color.dart';
+import 'package:learn_flutter_discuss_app/config/app_route.dart';
 
 Future<void> main() async {
   await initializeDateFormatting('id_ID');
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -13,16 +15,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primaryColor: AppColor.primary,
         colorScheme: const ColorScheme.light().copyWith(
           primary: AppColor.primary,
         ),
-        useMaterial3: true,
+        // useMaterial3: true,
       ),
-      home: const Scaffold(),
+      routerConfig: AppRoute.routeConfig,
     );
   }
 }
