e0fabe4e66ca43fe0b699f135b33d3b4f9f1a431
Muhammad Nurhuda Hendrian <mnurhuda009@gmail.com>
2023-08-11 13:52:10 +0700

    add user session

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'config/session.dart';
 import 'config/app_color.dart';
 import 'config/app_route.dart';
 import 'controllers/c_account.dart';
@@ -31,23 +32,30 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider<CMyTopic>(create: (_) => CMyTopic()),
         ChangeNotifierProvider<CAccount>(create: (_) => CAccount()),
       ],
-      child: MaterialApp.router(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primaryColor: AppColor.primary,
-          colorScheme: const ColorScheme.light().copyWith(
-            primary: AppColor.primary,
-            secondary: AppColor.primary,
+      builder: (context, child) {
+        Session.getUser().then((user) {
+          if (user != null) {
+            context.read<CUser>().data = user;
+          }
+        });
+        return MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primaryColor: AppColor.primary,
+            colorScheme: const ColorScheme.light().copyWith(
+              primary: AppColor.primary,
+              secondary: AppColor.primary,
+            ),
+            floatingActionButtonTheme: const FloatingActionButtonThemeData(
+              backgroundColor: AppColor.primary,
+              foregroundColor: Colors.white,
+            ),
+            // useMaterial3: true,
           ),
-          floatingActionButtonTheme: const FloatingActionButtonThemeData(
-            backgroundColor: AppColor.primary,
-            foregroundColor: Colors.white,
-          ),
-          // useMaterial3: true,
-        ),
-        routerConfig: AppRoute.routeConfig,
-      ),
+          routerConfig: AppRoute.routeConfig,
+        );
+      },
     );
   }
 }
